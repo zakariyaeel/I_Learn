47b4e14725a9b99f83fbd9f9018de2e2473919ec
zakariyae <98268057+zakariyaeel@users.noreply.github.com>
2025-02-16 23:37:14 +0100

    Create HelloWorld.java

---
+++ Java/first_code/HelloWorld.java
@@ -0,0 +1,9 @@
+@package pkg
+public class HelloWorld{
+  public static void main(String [] args){
+    System.out.println("Hello " + args[0])
+  }
+}
+
+// to execute the code type in the cmd this commande
+// cmd : java -cp "class path" pkg.HelloWorld "your name"
