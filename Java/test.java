282b008f89d032f5bc42ad784b35640c5db7b229
zakariyae <98268057+zakariyaeel@users.noreply.github.com>
2025-02-17 23:47:19 +0100

    Create test.java

---
+++ Java/test.java
@@ -0,0 +1,12 @@
+public class Main { // PascalNamingConvention
+    public static void main(String[] args) { // camelNamingConvention
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        for(int i=0;i<2;++i){
+            for(int j=0;j<3;++j){
+                System.out.print(i + " " + j + " ");
+            }
+        }
+        System.out.println("Hello and welcome!");
+    }
+}
