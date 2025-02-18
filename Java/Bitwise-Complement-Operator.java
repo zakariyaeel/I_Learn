bd79acb3fb38ce8b400dd14a24614fc6340d8e25
zakariyae <98268057+zakariyaeel@users.noreply.github.com>
2025-02-18 21:09:05 +0100

    Create Bitwise-Complement-Operator.java

---
+++ Java/Bitwise-Complement-Operator.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+  	int a = 500;
+    int b = ~a; // is the opposite and then adding 1 
+    System.out.println(b); // out : -501
+  }
+}
+
+// ~ bitwise operator (tilde)
