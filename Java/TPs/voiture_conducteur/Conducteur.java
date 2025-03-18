121e0875a1db18b3f1acafb378f6f56e1ae67691
zakariyae <98268057+zakariyaeel@users.noreply.github.com>
2025-03-18 00:23:18 +0000

    Create Conducteur.java

---
+++ Java/TPs/voiture_conducteur/Conducteur.java
@@ -0,0 +1,44 @@
+public class Conducteur {
+    private int id;
+    private String nom;
+    private String prenom;
+    private Voiture voiture;
+
+    public Conducteur(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Voiture getVoiture() {
+        return voiture;
+    }
+
+    public void setVoiture(Voiture voiture) {
+        this.voiture = voiture;
+    }
+}
