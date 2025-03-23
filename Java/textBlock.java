5fa0215e583cf51b4c228c1b1815b96d75fb7209
zakariyae <98268057+zakariyaeel@users.noreply.github.com>
2025-03-23 15:22:57 +0000

    Create textBlock.java

---
+++ Java/textBlock.java
@@ -0,0 +1,11 @@
+String txtBlkSample = """
+	String txtBlk = \"""
+	text block line 1
+	text block line 2. \n \""";
+""";
+String txtStrSample = """
+	String txtStr = " String value ";
+""";
+
+System.out.println(txtBlkSample );
+System.out.println(txtStrSample );
